149fdbee44d97df592fce401f4ba1f01504d7ba1
rogeliorv <rogeliorv@gmail.com>
2024-06-25 12:15:05 +0300

    chore: first working version

--- week-2-project/joke-telling-machine/src/pages/index.tsx
+++ week-2-project/joke-telling-machine/src/pages/index.tsx
@@ -1,6 +1,37 @@
 "use client";
 
+import { useState } from "react";
+import { useChat } from "ai/react";
+
+const genres = [
+  { emoji: "🧙", value: "Innocent" },
+  { emoji: "🕵️", value: "Math" },
+  { emoji: "💑", value: "Romance" },
+  { emoji: "🚀", value: "Sci-Fi" },
+];
+const tones = [
+  { emoji: "😊", value: "Trump" },
+  { emoji: "😊", value: "Stalin" },
+  { emoji: "😊", value: "Goku" },
+  { emoji: "😊", value: "Dua Lipa" },
+];
+
 export default function Chat() {
+  const { messages, append, isLoading } = useChat();
+
+  const [state, setState] = useState({
+    genre: "",
+    tone: "",
+  });
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
 
   return (
     <main className="mx-auto w-full p-24 flex flex-col">
@@ -13,13 +44,76 @@ export default function Chat() {
             </p>
           </div>
 
-          {/* genre selection code */}
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Genre</h3>
 
-          {/* tone selection code */}
+            <div className="flex flex-wrap justify-center">
+              {genres.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    value={value}
+                    name="genre"
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
 
-          {/* button code */}
+          <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+            <h3 className="text-xl font-semibold">Tones</h3>
 
-          {/* chat messages code */}
+            <div className="flex flex-wrap justify-center">
+              {tones.map(({ value, emoji }) => (
+                <div
+                  key={value}
+                  className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                >
+                  <input
+                    id={value}
+                    type="radio"
+                    name="tone"
+                    value={value}
+                    onChange={handleChange}
+                  />
+                  <label className="ml-2" htmlFor={value}>
+                    {`${emoji} ${value}`}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+            disabled={isLoading || (!state.genre || !state.tone)}
+            onClick={() =>
+              append({
+                role: "user",
+                content: `Generate a ${state.genre} joke in a ${state.tone} tone`,
+              })
+            }
+          >
+            Generate joke
+          </button>
+
+          <div
+            hidden={
+              messages.length === 0 ||
+              messages[messages.length - 1]?.content.startsWith("Generate")
+            }
+            className="bg-opacity-25 bg-gray-700 rounded-lg p-4"
+          >
+            {messages[messages.length - 1]?.content}
+          </div>
         </div>
       </div>
     </main>
