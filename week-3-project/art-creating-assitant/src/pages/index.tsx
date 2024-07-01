167864cb8e4e689ed95c05aae0ec7109e306eb38
rogeliorv <rogeliorv@gmail.com>
2024-07-01 13:26:10 +0300

    chore: added pickable sizes

--- week-3-project/art-creating-assitant/src/pages/index.tsx
+++ week-3-project/art-creating-assitant/src/pages/index.tsx
@@ -3,7 +3,7 @@
 import { useMemo, useState } from "react";
 import { TextDelta, Text } from "openai/resources/beta/threads/messages.mjs";
 import { ArtAssistant } from "@/utils/assistant";
-import { ImageGenerator } from "@/utils/imageGeneration";
+import { ImageGenerator, SizeType } from "@/utils/imageGeneration";
 
 export default function Chat() {
   const assistant = useMemo(() => new ArtAssistant(), []);
@@ -12,6 +12,7 @@ export default function Chat() {
   const [error, setError] = useState<string>('');
   const [isImageLoading, setIsImageLoading] = useState<boolean>(false);
   const [imgUrl, setImgUrl] = useState<string>('');
+  const [imgSize, setImgSize] = useState<SizeType>('1024x1024');
 
   const [chatBoxText, setChatBoxText] = useState<string>('');
   const [artDescription, setArtDescription] = useState<string>('');
@@ -71,17 +72,24 @@ export default function Chat() {
 
             {imgUrl && <img src={imgUrl} />}
 
+            <p>Choose Size: {imgSize}</p>
+            <div className="flex flex-row gap-x-3 gap-y-3">
+              <button className='bg-slate-600 px-2 py-2 rounded-lg' onClick={() => setImgSize('1024x1024')}>1024x1024</button>
+              <button className='bg-slate-600 px-2 py-2 rounded-lg' onClick={() => setImgSize('1024x1792')}>1024x1792</button>
+              <button className='bg-slate-600 px-2 py-2 rounded-lg' onClick={() => setImgSize('1792x1024')}>1792x1024</button>
+            </div>
+
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
               disabled={(!artDescription || isImageLoading)}
               onClick={() => {
                 setError('');
                 setIsImageLoading(true);
-                imageGenerator.generateImage(artDescription).then((imgUrl: string) => {
+                imageGenerator.generateImage(artDescription, imgSize).then((imgUrl: string) => {
                   setImgUrl(imgUrl);
                   setIsImageLoading(false);
                 }).catch((error) => {
-                  setError(error);
+                  setError(error.message);
                   setIsImageLoading(false);
                 });
               }}
--- week-3-project/art-creating-assitant/src/utils/imageGeneration.ts
+++ week-3-project/art-creating-assitant/src/utils/imageGeneration.ts
@@ -1,5 +1,8 @@
 import OpenAI from "openai";
 
+
+export type SizeType = '1024x1024' | '1792x1024' | '1024x1792' | null;
+
 export class ImageGenerator {
 
   private openAI;
@@ -8,16 +11,15 @@ export class ImageGenerator {
     this.openAI = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser:  true});
   }
 
-  generateImage = async (prompt: string) => {
+  generateImage = async (prompt: string, size: SizeType) => {
 
     const response = await this.openAI.images.generate({
       model: "dall-e-3",
       prompt: prompt,
       n: 1,
-      size: "1024x1024",
+      size: size,
     });
 
-    console.log(response.data);
     const imgUrl = response.data[0].url || '';
     return imgUrl;
   }
